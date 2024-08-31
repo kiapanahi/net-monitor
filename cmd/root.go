467867e7d87711159c05d089369f5e7ada64efae
Kia Raad <kia.panahirad@gmail.com>
2024-08-31 22:30:55 +0330

    Update the commands to use zerolog for logging.
    Replace all usages of `log` with `zerolog` in the commands.

--- cmd/root.go
+++ cmd/root.go
@@ -3,9 +3,14 @@ package cmd
 import (
 	"fmt"
 	"os"
+	"time"
 
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
 	"github.com/spf13/cobra"
 )
+
+var logLevel int32
 
 var rootCommand = &cobra.Command{
 	Use:   "net-monitor",
@@ -16,6 +21,12 @@ var rootCommand = &cobra.Command{
 }
 
 func Execute() {
+	log.Logger = zerolog.New(zerolog.ConsoleWriter{Out: os.Stderr, TimeFormat: time.RFC3339}).
+		Level(zerolog.Level(logLevel)).
+		With().
+		Timestamp().
+		Logger()
+
 	if err := rootCommand.Execute(); err != nil {
 		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
@@ -23,5 +34,7 @@ func Execute() {
 }
 
 func init() {
+	rootCommand.PersistentFlags().Int32VarP(&logLevel, "log-level", "l", -1, "Log levels.\n-1: Trace\n0: Debug\n1: Info\n2: Warn\n3: Error\n4: Fatal\n5: Panic")
 	rootCommand.AddCommand(monitorCmd)
+
 }
